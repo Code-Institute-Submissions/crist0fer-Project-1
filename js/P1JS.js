39aee315b5c059ee2322c9f1ef3c50deedd66edf
Chris Styan <chrisstyan44@outlook.com>
2018-03-26 17:26:37 +0100

    repository clear up and requested changes

---
+++ js/P1JS.js
@@ -0,0 +1,45 @@
+        $(document).ready(function() {
+            $(".gallery .right.carousel-control").on("click",function (e) {
+              var currentIndex = $(".preview").data("current-index");
+              var nextIndex = currentIndex +1;
+
+              var nextThumbnail = $( $(".thumbnail img").get(nextIndex) );
+              $(".preview").prop("src", nextThumbnail.data("src"));
+              $(".preview").data("current-index", nextIndex);
+
+             $(".formbox").click(function() 
+
+        {
+
+
+          alert("Thank You, we look forward to giving you a quote.");
+
+
+           });
+           });
+
+
+             $(".gallery .left.carousel-control").on("click",function (e) {
+              var currentIndex = $(".preview").data("current-index");
+              var previousIndex = currentIndex -1;
+
+              var previousThumbnail = $( $(".thumbnail img").get(previousIndex) );
+              $(".preview").prop("src", previousThumbnail.data("src"));
+              $(".preview").data("current-index", previousIndex);
+
+
+            });
+
+
+            $("#thumbnails img").on("click",function (e) {
+                var gallerythumbnail = $(this);
+                var currentIndex = $(".thumbnail img").index( gallerythumbnail );
+                $(".preview").prop("src", gallerythumbnail.data("src"));
+                $(".preview").data("current-index", currentIndex);
+
+            });
+        });
+
+
+
+
